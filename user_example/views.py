de36f18e5b0d440cf49109d044b08b13d8a17fbd
rizalw <rizalwidyananda@gmail.com>
2022-04-05 14:32:38 +0700

    finished

---
+++ user_example/views.py
@@ -0,0 +1,24 @@
+
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+
+# Create your views here.
+def index(request):
+    return render(request, 'user_example/index.html')
+
+def register(request):
+    if request.POST:
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    context = {'form' : form}
+    return render(request, 'registration/register.html', context)
